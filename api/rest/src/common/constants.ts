87d2305acf71b45bc2be951f56da1001257e63da
Raja Junaid <rajajunaid@Tests-MacBook-Pro.local>
2023-03-11 09:29:19 +0530

    first commit for url change

--- api/rest/src/common/constants.ts
+++ api/rest/src/common/constants.ts
@@ -1,2 +1,2 @@
 // export const APP_URL = 'http://localhost:5000/api';
-export const APP_URL = 'https://temp-seller-be.onrender.com/api';
+export const APP_URL = 'https://seller-temp-be.onrender.com/api';
